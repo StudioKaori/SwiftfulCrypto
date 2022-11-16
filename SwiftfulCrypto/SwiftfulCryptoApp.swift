0d0f03c5baf7a392373e2f17bc44d829a7c13488
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-11-16 15:36:29 +0100

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Kaori Persson on 2022-11-16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
