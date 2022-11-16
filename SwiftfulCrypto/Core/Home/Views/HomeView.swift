7c77a8fca23c8a2285cfec506298808814a52cb4
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-11-16 21:57:00 +0100

    add animation to the circle button

--- SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -8,28 +8,48 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
-      ZStack {
-        // background layer
-        Color.theme.background
-          .ignoresSafeArea()
-        
-        // content layer
-        VStack {
-          Text("Header")
-          
-          // By setting spacer here, it can push up the header to the top although minLength is 0
-          Spacer(minLength: 0)
+  
+  @State private var showPortfolio: Bool = false
+  
+  var body: some View {
+    ZStack {
+      // background layer
+      Color.theme.background
+        .ignoresSafeArea()
+      
+      // content layer
+      VStack {
+        HStack {
+          CircleButtonView(iconName: "info")
+          Spacer()
+          Text("Live prices")
+            .font(.headline)
+            .fontWeight(.heavy)
+            .foregroundColor(Color.theme.accent)
+          Spacer()
+          CircleButtonView(iconName: "chevron.right")
+            .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+            .onTapGesture {
+              withAnimation(.spring()) {
+                showPortfolio.toggle()
+              }
+            }
         }
+        .padding(.horizontal)
+        
+        
+        // By setting spacer here, it can push up the header to the top although minLength is 0
+        Spacer(minLength: 0)
       }
     }
+  }
 }
 
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-      NavigationView {
-        HomeView()
-          .navigationBarBackButtonHidden(true)
-      }
+  static var previews: some View {
+    NavigationView {
+      HomeView()
+        .navigationBarBackButtonHidden(true)
     }
+  }
 }
