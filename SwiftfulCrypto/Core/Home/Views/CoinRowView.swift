1bac95f3b80cb929bf050a312191e5ec8a463c1e
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-11-17 19:14:09 +0100

    Add preview provider

---
+++ SwiftfulCrypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,23 @@
+//
+//  CoinRowView.swift
+//  SwiftfulCrypto
+//
+//  Created by Kaori Persson on 2022-11-17.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+  
+  //let coin: CoinModel
+  
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView()
+    }
+}
