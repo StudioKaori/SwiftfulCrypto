3c4f8fe933eee587afc2d6e4e37cc51cb59ccf65
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-11-25 16:59:07 +0100

    Add vm

--- SwiftfulCrypto/Core/Home/Components/CoinImage/CoinImageView.swift
+++ SwiftfulCrypto/Core/Home/Components/CoinImage/CoinImageView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 class CoinImageViewModel: ObservableObject {
   
   @Published var image: UIImage? = nil
-  @Published var isLoading: Bool = false
+  @Published var isLoading: Bool = true
   
   init() {
     getImage()
@@ -24,14 +24,28 @@ class CoinImageViewModel: ObservableObject {
 
 struct CoinImageView: View {
   
-  @Published var vm = CoinImageViewModel()
+  @StateObject var vm: CoinImageViewModel = CoinImageViewModel()
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      ZStack {
+        if let image = vm.image {
+          Image(uiImage: image)
+            .resizable()
+            .scaledToFit()
+        } else if vm.isLoading {
+          ProgressView()
+        } else {
+          Image(systemName: "questionmark")
+            .foregroundColor(Color.theme.secondaryText)
+        }
+      }
     }
 }
 
 struct CoinImageView_Previews: PreviewProvider {
     static var previews: some View {
         CoinImageView()
+        .previewLayout(.sizeThatFits)
+        .padding()
     }
 }
